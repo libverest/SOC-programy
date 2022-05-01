f98ddc592b1f9b8b0d00366613f4332e1f8d7d36
libverest <samuel.kotrla@gmail.com>
2022-05-01 12:09:30 +0200

    Added Arduino code

---
+++ sketch_feb14c/sketch_feb14c.ino
@@ -0,0 +1,26 @@
+int input1=9;
+
+void setup() {
+ pinMode(input1, OUTPUT);
+ Serial.begin(9600);
+ }
+ 
+void loop() {
+  
+ if(Serial.available()>0)
+   {     
+      char data= Serial.read(); 
+      switch(data)
+      {
+        case 'a': digitalWrite(input1, HIGH);
+        break;
+        
+        case 'b': digitalWrite(input1, LOW);
+        break;
+        
+        default : break;
+      }
+      Serial.println(data);
+   }
+   delay(50);
+}
